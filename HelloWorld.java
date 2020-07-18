c30d6a7771b5723cdd3d587c83412485553675fc
David V Phan <66977229+Phantom0001@users.noreply.github.com>
2020-07-17 19:39:56 -0500

    0: HelloWorld
    Added first program into github, 0: HelloWorld!

---
+++ HelloWorld.java
@@ -0,0 +1,17 @@
+/**
+ *  Name: David Phan
+ *
+ * Project/Class Description:
+ * This program will print out Hello, World.
+ *
+ * Known bugs: None
+ */
+public class HelloWorld {
+    // This is the main method. It will print out Hello, World.
+    public static void main(String[] args) {
+
+        // This will print out Hello, World.
+        System.out.println("Hello, World.");
+
+    }
+}
